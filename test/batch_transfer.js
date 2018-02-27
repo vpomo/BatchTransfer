9fb42d69abd884ca6cd5ef62d76c359f31e8dea5
vpomo <pomogalov@gmail.com>
2018-02-28 00:14:45 +0300

    +web

--- test/batch_transfer.js
+++ test/batch_transfer.js
@@ -1,5 +1,10 @@
 var BatchTransfer = artifacts.require("./BatchTransfer.sol");
 //import assertRevert from './helpers/assertRevert';
+//https://ropsten.etherscan.io/address/0xc611d5b26e3c750b5ea973d4c7c7b3ab8f49d8f6#code
+
+//https://ropsten.etherscan.io/address/0x556e0c6f38cceac9241b2ea507f424b8286d4040
+
+//https://hackernoon.com/how-to-script-an-automatic-token-airdrop-for-40k-subscribers-e40c8b1a02c6
 
 contract('BatchTransfer', (accounts) => {
     var contract;
@@ -13,6 +18,13 @@ contract('BatchTransfer', (accounts) => {
 
     it('get address contract', async ()  => {
         assert.notEqual(undefined, contract.address);
+    });
+
+    it('check batch transfer', async ()  => {
+        await contract.batchTransfer(["0x111111","0x222222"],[2000,3000]);
+        var balanceAccountOne = await contract.balanceOf("0x111111");
+        console.log("balanceAccountOne = " + balanceAccountOne);
+        //assert.notEqual(undefined, contract.address);
     });
 
 });
---
+++ web/metaScript.js
@@ -0,0 +1,62 @@
+window.addEventListener('load', function() {
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
+    // Use Mist/MetaMask's provider
+    console.log("Web3 detected!");
+    window.web3 = new Web3(web3.currentProvider);
+    // Now you can start your app & access web3 freely:
+    var currentNetwork = web3.version.network;
+    if (currentNetwork == 3){
+        $('#dappInfo').html("You are connected to the Blockchain");
+    } else {
+        $('#dappInfo').html("You are not connected to the Ropsten network");
+    }
+    startApp();
+  } else {
+    $('#dappInfo').html("Please use Chrome or Firefox, install the Metamask extension and retry the request!");
+    //alert('Please use Chrome or Firefox, install Metamask and then try again!')
+  }
+})
+
+function startApp() {
+  var myWalletAddress = web3.eth.accounts[0];
+  var contract = initContract();
+
+/*
+  contract.balanceOf(myWalletAddress, function(error, data) {
+    $('#walletTokens').html(Number(data)/10**18);
+	document.getElementById('numberTokens').value = Number(data)/10**18;
+  });
+*/
+
+}
+
+$(document).ready(function(){
+//https://developers.google.com/chart/interactive/docs/gallery/piechart
+});
+
+function batchTransfer() {
+    var addressVoteOne = "0x2AB09D4BCcC9D8Ca27D3ACc8BD7c15861a08C1F3";
+    var contract = initContract();
+
+    contract.batchTransfer(["0x333333", "0x444444"],[2000000000000000,2000000000000000000], function(error, data) {
+      //$('#walletTokens').html(Number(data)/10**18);
+  	  //document.getElementById('numberTokens').value = Number(data)/10**18;
+  	  console.log("data = " + data);
+    });
+
+}
+
+function initContract(){
+ var address = {
+    "3" : "0xc611D5B26e3C750B5eA973D4C7C7B3Ab8f49d8f6" // Ropsten
+  }
+  var current_network = web3.version.network;
+  var abi = [{"constant":false,"inputs":[{"name":"_addressContract","type":"address"}],"name":"setContractToken","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"userTransfered","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"standardToken","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_recipients","type":"address[]"},{"name":"_values","type":"uint256[]"}],"name":"batchTransfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_newOwner","type":"address"}],"name":"changeOwner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_admin","type":"address"},{"name":"_isAdmin","type":"bool"}],"name":"setContractAdmin","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"contractAdmins","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"_owner","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"TransferToken","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnerChanged","type":"event"}];
+  var contract = web3.eth.contract(abi).at(address[current_network]);
+  console.log("Contract initialized successfully");
+  console.log("current_network = " + current_network);
+
+  return contract;
+
+}
